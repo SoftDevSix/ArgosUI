c85d4791c64647c57a060125db0495084206b8ab
fabian-romero-jalafund <fabian.romero@fundacion-jala.org>
2024-11-20 07:55:08 -0400

    chore: add coverage set up

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
     globals: true,
+    coverage: {
+      provider: "v8", 
+      reporter: ["text", "json", "html", "lcov"],
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+      exclude: ["src/**/index.ts", "src/main.tsx", "src/vite-env.d.ts"], 
+    },
   },
 });
