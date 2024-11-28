eaa61340c4e33dc06800584be0bbc3831795e2df
Emanuel Galindo <emanuel.galindo.9362056@usalesiana.edu.bo>
2024-11-27 21:32:04 -0400

    feat: post to file manager

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,26 @@ import Error from "./pages/Error";
 import theme from "./utils/theme";
 import Header from "./components/Header";
 import ProjectCoveragePage from "./pages/ProjectCoverage";
+import { UploadedKeysProvider } from "./context/UploadedKeysContext";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-        <Header />
-        <Container>
-          <Routes>
-            <Route path={PageNames.HOME} Component={Home} />
-            <Route
-              path={PageNames.PROJECT_COVERAGE}
-              Component={ProjectCoveragePage}
-            />
-            <Route path={PageNames.ERROR_404} Component={Error} />
-          </Routes>
-        </Container>
+        <UploadedKeysProvider>
+          <Header />
+          <Container>
+            <Routes>
+              <Route path={PageNames.HOME} Component={Home} />
+              <Route
+                path={PageNames.PROJECT_COVERAGE}
+                Component={ProjectCoveragePage}
+              />
+              <Route path={PageNames.ERROR_404} Component={Error} />
+            </Routes>
+          </Container>
+        </UploadedKeysProvider>
       </BrowserRouter>
     </ThemeProvider>
   );
